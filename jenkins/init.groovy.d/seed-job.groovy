8cab187081164f9ddb3d93988912355c042c7b50
Ant0n10x78 <antonio.gernacaleixo@ynov.com>
2025-04-23 12:15:23 +0200

    update

--- jenkins/init.groovy.d/seed-job.groovy
+++ jenkins/init.groovy.d/seed-job.groovy
@@ -1,22 +1,30 @@
 import jenkins.model.*
 import hudson.model.*
-import javaposse.jobdsl.plugin.ExecuteDslScripts
+import javaposse.jobdsl.plugin.*
 
-println "--> Création du job 'pipeline' à partir d’un seed"
+def instance = Jenkins.instance
 
-def instance = Jenkins.getInstance()
-
-def jobName = "pipeline"
+def jobName = "seed"
 def job = instance.getItem(jobName)
 
 if (job == null) {
+    println "--> Création du job ${jobName}"
+
     def seedJob = instance.createProject(FreeStyleProject, jobName)
-    seedJob.buildersList.add(new ExecuteDslScripts(
-        new ExecuteDslScripts.ScriptLocation("true", "", "/var/jenkins_home/jobsdsl/pipelineJob.groovy"),
-        false, 
-        RemovedJobAction.IGNORE, 
-        RemovedViewAction.IGNORE, 
-        LookupStrategy.JENKINS_ROOT
-    ))
+    
+    def dslScript = new ExecuteDslScripts()
+    dslScript.targets = "jobs/pipeline/*.groovy" //"/var/jenkins_home/jobs/pipeline/*.groovy"
+    dslScript.usingScriptText = false
+    dslScript.ignoreExisting = false
+    dslScript.removedJobAction = RemovedJobAction.IGNORE
+    dslScript.removedViewAction = RemovedViewAction.IGNORE
+    dslScript.lookupStrategy = LookupStrategy.JENKINS_ROOT
+    dslScript.additionalClasspath = ""
+
+    seedJob.buildersList.add(dslScript)
     seedJob.save()
+
+    println "--> Job ${jobName} créé avec DSL, prêt à être lancé manuellement."
+} else {
+    println "--> Job ${jobName} existe déjà"
 }
